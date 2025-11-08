1cfde743c9388739cf44a5f3bdf97c4454acb58d
Daniel Gao <daniel.gao.work@gmail.com>
2025-11-07 21:58:02 -0500

    Add folke/snacks.nvim

--- .config/nvim/lua/plugins/util.lua
+++ .config/nvim/lua/plugins/util.lua
@@ -1,3 +1,26 @@
 return {
-  { "nvim-tree/nvim-web-devicons", lazy = true }
+  { "nvim-tree/nvim-web-devicons", lazy = true },
+  {
+    "folke/snacks.nvim",
+    priority = 9001,
+    lazy = false,
+    opts = {
+      bigfile = { enabled = true },
+      dashboard = { enabled = false },
+      explorer = { enabled = false },
+      indent = { enabled = false },
+      input = { enabled = false },
+      picker = { enabled = false },
+      notifier = { enabled = false },
+      quickfile = { enabled = true },
+      scope = { enabled = false },
+      scroll = { enabled = false },
+      statuscolumn = { enabled = false },
+      words = { enabled = false },
+    },
+    keys = {
+      { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
+      { "<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File" },
+    }
+  }
 }
